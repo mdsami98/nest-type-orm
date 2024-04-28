33172cbc871e3a751a181d1818070dedbef38714
mdsami9898 <mdsamiwork9898@gmail.com>
2024-04-29 01:29:39 +0600

    besic swager added

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Report Module')
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  @Get('/')
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  getData(): string {
+    return 'report';
+  }
+}
